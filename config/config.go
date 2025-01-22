8ea001ce146ee85d82393629f3bab795bd4b78b6
Kamila <113854806+KarmaBeLike@users.noreply.github.com>
2025-01-22 18:21:25 +0500

    docker is fixed

--- config/config.go
+++ config/config.go
@@ -13,8 +13,8 @@ func New() *Config {
 	return &Config{
 		MorningHour: 10,
 		MorningMin:  37,
-		NoonHour:    16,
-		NoonMin:     11,
+		NoonHour:    18,
+		NoonMin:     25,
 		EveningHour: 21,
 		EveningMin:  59,
 	}
