aa9f5022fd1db607fef92455f59a1a3bc7c21634
Kamila <113854806+KarmaBeLike@users.noreply.github.com>
2025-02-07 18:06:04 +0500

    test

--- config/config.go
+++ config/config.go
@@ -15,8 +15,8 @@ func New() *Config {
 	tz, _ := time.LoadLocation("Asia/Atyrau")
 	return &Config{
 		TimeZone:    tz,
-		MorningTime: time.Date(0, 0, 0, 21, 10, 0, 0, tz),
-		NoonTime:    time.Date(0, 0, 0, 21, 13, 0, 0, tz),
-		EveningTime: time.Date(0, 0, 0, 21, 15, 0, 0, tz),
+		MorningTime: time.Date(0, 0, 0, 18, 10, 0, 0, tz),
+		NoonTime:    time.Date(0, 0, 0, 18, 13, 0, 0, tz),
+		EveningTime: time.Date(0, 0, 0, 18, 15, 0, 0, tz),
 	}
 }
